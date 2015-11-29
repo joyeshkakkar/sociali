dd22fb60fd135ae6279a47f34c7696425543515a
Abhishek Ravi Chandran <abhi2488@ccs.neu.edu>
2015-11-29 04:22:55 -0500

    fetch events on login, and change markers when search is performed

--- public/views/events/eventsCtrl.js
+++ public/views/events/eventsCtrl.js
@@ -1,82 +1,113 @@
 app.controller("EventsController", function ($scope, $http, $rootScope, $location,uiGmapGoogleMapApi,$templateCache) {
-    //setting up the map
+
     var map;
     var markers = [];
-    var myCenter = new google.maps.LatLng(40.69847032728747,
-        -73.9514422416687);
-    var browserSupportFlag = new Boolean();
 
-    //initializing the map
-    function initialize() {
-        var mapProp = {
-            center : myCenter,
-            zoom : 10,
-            mapTypeId : google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map"),
-            mapProp);
-        //getting users current location
-        if (navigator.geolocation) {
-            browserSupportFlag = true;
-            navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    initialLocation = new google.maps.LatLng(
-                        position.coords.latitude,
-                        position.coords.longitude);
-                    map.setCenter(initialLocation);
-                    var marker = new google.maps.Marker({
-                        animation: google.maps.Animation.DROP,
-                        position : initialLocation,
-                        title: 'lat:'+position.coords.latitude+' long:'+position.coords.longitude,
-                        //icon : 'images/blue.png'
-                    });
-                    marker.setMap(map);
-                    //to zoom
-                    google.maps.event.addListener(marker, 'click',
-                        function() {
-                            map.setZoom(15);
-                            map.setCenter(marker.getPosition());
+    //method to load events as soon as user logs in
+    $scope.loadUserEvents = function(){
+        //setting up the map
+
+
+        var myCenter = new google.maps.LatLng(40.69847032728747,
+            -73.9514422416687);
+        var browserSupportFlag = new Boolean();
+
+        //initializing the map
+        //function initialize() {
+            var mapProp = {
+                center : myCenter,
+                zoom : 10,
+                mapTypeId : google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(document.getElementById("map"),
+                mapProp);
+            //getting users current location
+            if (navigator.geolocation) {
+                browserSupportFlag = true;
+                navigator.geolocation.getCurrentPosition(
+                    function(position) {
+                        $scope.latitude=position.coords.latitude;
+                        $scope.longitude=position.coords.longitude;
+                        initialLocation = new google.maps.LatLng(
+                            position.coords.latitude,
+                            position.coords.longitude);
+                        map.setCenter(initialLocation);
+                        var marker = new google.maps.Marker({
+                            animation: google.maps.Animation.DROP,
+                            position : initialLocation,
+                            title: 'lat:'+position.coords.latitude+' long:'+position.coords.longitude,
+                            //icon : 'images/blue.png'
                         });
+                        marker.setMap(map);
+                        //to zoom
+                        google.maps.event.addListener(marker, 'click',
+                            function() {
+                                map.setZoom(15);
+                                map.setCenter(marker.getPosition());
+                            });
 
-                }, function() {
-                    handleNoGeolocation(browserSupportFlag);
-                });
-        }
-        // Browser doesn't support Geolocation
-        else {
-            browserSupportFlag = false;
-            handleNoGeolocation(browserSupportFlag);
-        }
-        function handleNoGeolocation(errorFlag) {
-            if (errorFlag == true) {
-                alert("Geolocation service failed.");
-                initialLocation = myCenter;
-            } else {
-                alert("Your browser doesn't support geolocation. We've placed you in newyork.");
-                initialLocation = myCenter;
+                    }, function() {
+                        handleNoGeolocation(browserSupportFlag);
+                    });
             }
-            map.setCenter(initialLocation);
-            var marker = new google.maps.Marker({
-                position : initialLocation,
-                icon : 'images/green.png'
-            });
-            marker.setMap(map);
-            //to zoom
-            google.maps.event.addListener(marker, 'click', function() {
-                map.setZoom(15);
-                map.setCenter(marker.getPosition());
-            });
-        }
+            // Browser doesn't support Geolocation
+            else {
+                browserSupportFlag = false;
+                handleNoGeolocation(browserSupportFlag);
+            }
+            function handleNoGeolocation(errorFlag) {
+                alert(errorFlag);
+                if (errorFlag == true) {
+                    alert("Geolocation service failed.");
+                    initialLocation = myCenter;
+                } else {
+                    alert("Your browser doesn't support geolocation. We've placed you in newyork.");
+                    initialLocation = myCenter;
+                }
+                map.setCenter(initialLocation);
+                var marker = new google.maps.Marker({
+                    position : initialLocation,
+                    icon : 'images/green.png'
+                });
+                marker.setMap(map);
+                //to zoom
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.setZoom(15);
+                    map.setCenter(marker.getPosition());
+                });
+            }
+        //}
+        //google.maps.event.addDomListener(window, 'load', initialize);
+        var url= 'https://www.eventbriteapi.com/v3//events/search/';
+        var token = 'JJJKFTCUFVWB2HPKT2DS';
+        var token2 = 'QC44X66MUP27NDX7MDZL';
+        var location = '&location.within=5mi&location.latitude='+'42.3372703'+'&location.longitude='+'-71.0913595';
+
+        //setting category 103--music
+        var searchQuery = url + '?categories=103' + location +
+            '&token=' + token + '&expand=venue';
+        $scope.code = null;
+        $scope.response = null;
+        $scope.method = 'GET';
+
+        //to fetch all the events
+        $http({method: $scope.method, url: searchQuery, cache: $templateCache}).
+        then(function(response) {
+            $scope.status = response.status;
+            $scope.data = response.data;
+            if($scope.data != null)
+                resetMarkers();
+        }, function(response) {
+            $scope.data = response.data || "Request failed";
+            $scope.status = response.status;
+        });
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-
-    //method to fetch the events
+    //method to fetch the events searced for
     $scope.fetch = function() {
         var url= 'https://www.eventbriteapi.com/v3//events/search/';
         var token = 'JJJKFTCUFVWB2HPKT2DS';
         var token2 = 'QC44X66MUP27NDX7MDZL';
-        var location = '&location.within=5mi&location.latitude=42.3372703&location.longitude=-71.0913595';
+        var location = '&location.within=5mi&location.latitude='+$scope.latitude+'&location.longitude='+$scope.longitude;
         //*var batchurl = "https://www.eventbriteapi.com/v3/batch/" + '?token=' + token;
         //var venueURL = '{"method":"GET", "relative_url":"venues/';
 
@@ -94,7 +125,7 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
             $scope.status = response.status;
             $scope.data = response.data;
             if($scope.data != null)
-                setMarkers();
+                resetMarkers();
         }, function(response) {
             $scope.data = response.data || "Request failed";
             $scope.status = response.status;
@@ -106,6 +137,7 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
     //to set event markers
     function setMarkers() {
         for (var i = 0; i < $scope.data.events.length; i++) {
+
             var event = $scope.data.events[i];
             if (event.venue != null) {
                 var infowindow = new google.maps.InfoWindow({
@@ -125,6 +157,16 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
                 markers.push(marker);
             }
         }
+    }
+
+    //reset any previous markers if available
+    function resetMarkers(){
+        for (var i=0; i<markers.length; i++) {
+
+            markers[i].setMap(null);
+        }
+        markers = [];
+        setMarkers();
     }
 
 });
