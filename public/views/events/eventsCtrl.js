1d515a23adb746efcf50a08850e8047a280cb5fd
Abhishek Ravi Chandran <abhi2488@ccs.neu.edu>
2015-12-05 01:44:33 -0500

    pagination changes

--- public/views/events/eventsCtrl.js
+++ public/views/events/eventsCtrl.js
@@ -1,176 +1,201 @@
-app.controller("EventsController", function ($scope, $http, $rootScope, $location,$templateCache) {
+app.controller("EventsController", function ($scope, $http, $rootScope, $location, $templateCache) {
 
+    //will hold the map instance for the page
     var map;
+    //will hold the user location marker
     var userMarker;
+    //will hold all the envent markers
     var markers = [];
     var loadEvents = true;
+    //holds google directions service object
     var directionsService;
+    //holds the directions display object
     var directionsDisplay;
+
     //method to load events as soon as user logs in
-    $scope.loadUserEvents = function(){
-        //setting up the map]
+    $scope.loadUserEvents = function () {
 
-            var myCenter = new google.maps.LatLng(40.69847032728747,
-                -73.9514422416687);
-            var browserSupportFlag = new Boolean();
+        //setting up the map
+        //default center to be used if getting geo location fails
+        var myCenter = new google.maps.LatLng(42.3389,
+            -71.0903);
+        var browserSupportFlag = new Boolean();
 
-            //initializing the map
-            //function initialize() {
-            var mapProp = {
-                center : myCenter,
-                zoom : 12,
-                mapTypeId : google.maps.MapTypeId.ROADMAP
-            };
-            map = new google.maps.Map(document.getElementById("map"),
-                mapProp);
-            //getting users current location
-            if (navigator.geolocation) {
-                browserSupportFlag = true;
-                navigator.geolocation.getCurrentPosition(
-                    function(position) {
-                        $scope.latitude=position.coords.latitude;
-                        $scope.longitude=position.coords.longitude;
-                        initialLocation = new google.maps.LatLng(
-                            position.coords.latitude,
-                            position.coords.longitude);
-                        map.setCenter(initialLocation);
-                        var marker = new google.maps.Marker({
-                            animation: google.maps.Animation.DROP,
-                            position : initialLocation,
-                            title: 'lat:'+position.coords.latitude+' long:'+position.coords.longitude,
-                            //icon : 'images/blue.png'
-                        });
-                        marker.setMap(map);
-                        userMarker = marker;
-                        //to zoom
-                        google.maps.event.addListener(marker, 'click',
-                            function() {
-                                map.setZoom(15);
-                                map.setCenter(marker.getPosition());
-                            });
-                        fetchUserEvents();
-                    }, function() {
-                        handleNoGeolocation(browserSupportFlag);
-                    });
-            }
-            // Browser doesn't support Geolocation
-            else {
-                browserSupportFlag = false;
-                handleNoGeolocation(browserSupportFlag);
-            }
-            function handleNoGeolocation(errorFlag) {
-                alert("NoGeoLocation-->"+errorFlag);
-                if (errorFlag == true) {
-                    alert("Geolocation service failed.");
-                    initialLocation = myCenter;
-                } else {
-                    alert("Your browser doesn't support geolocation. We've placed you in newyork.");
-                    initialLocation = myCenter;
-                }
-                map.setCenter(initialLocation);
-                var marker = new google.maps.Marker({
-                    position : initialLocation,
-                    icon : 'images/green.png'
-                });
-                userMarker = marker;
-                marker.setMap(map);
-                //to zoom
-                google.maps.event.addListener(marker, 'click', function() {
-                    map.setZoom(15);
-                    map.setCenter(marker.getPosition());
-                });
-                fetchUserEvents();
-            }
-            //}
-
+        //initializing the map
+        var mapProp = {
+            center: myCenter,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+        //creating the map object
+        map = new google.maps.Map(document.getElementById("map"),
+            mapProp);
 
-    function fetchUserEvents(){
-        //google.maps.event.addDomListener(window, 'load', initialize);
-        var url= 'https://www.eventbriteapi.com/v3//events/search/';
+        //check if geo location is supported
+        if (navigator.geolocation) {
+            browserSupportFlag = true;
+
+            //getting users current location
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    $scope.latitude = position.coords.latitude;
+                    $scope.longitude = position.coords.longitude;
+                    initialLocation = new google.maps.LatLng(
+                        position.coords.latitude,
+                        position.coords.longitude);
+                    //setting map's center
+                    map.setCenter(initialLocation);
+                    //creating user's marker
+                    var marker = new google.maps.Marker({
+                        animation: google.maps.Animation.DROP,
+                        position: initialLocation,
+                        title: 'lat:' + position.coords.latitude + ' long:' + position.coords.longitude,
+                        //icon : 'images/blue.png'
+                    });
+                    marker.setMap(map);
+                    userMarker = marker;
+                    //to zoom on click
+                    google.maps.event.addListener(marker, 'click',
+                        function () {
+                            map.setZoom(15);
+                            map.setCenter(marker.getPosition());
+                        });
+                    fetchUserEvents();
+                }, function () {
+                    handleNoGeolocation(browserSupportFlag);
+                });
+        }
+        // Browser doesn't support Geolocation
+        else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+        }
+        function handleNoGeolocation(errorFlag) {
+            alert("NoGeoLocation-->" + errorFlag);
+            if (errorFlag == true) {
+                alert("Geolocation service failed.");
+                initialLocation = myCenter;
+            } else {
+                alert("Your browser doesn't support geolocation. We've placed you in newyork.");
+                initialLocation = myCenter;
+            }
+            map.setCenter(initialLocation);
+            var marker = new google.maps.Marker({
+                position: initialLocation,
+                icon: 'images/green.png'
+            });
+            userMarker = marker;
+            marker.setMap(map);
+            //to zoom
+            google.maps.event.addListener(marker, 'click', function () {
+                map.setZoom(15);
+                map.setCenter(marker.getPosition());
+            });
+            fetchUserEvents();
+        }
+
+        //}
+
+    };
+
+    //method to fetch user events on user login based on preferences
+    function fetchUserEvents() {
+        var url = 'https://www.eventbriteapi.com/v3//events/search/';
         var token = 'JJJKFTCUFVWB2HPKT2DS';
         var token2 = 'QC44X66MUP27NDX7MDZL';
-        var location = '&location.within=5mi&location.latitude='+$scope.latitude+'&location.longitude='
-            +$scope.longitude+'&popular=on';
+        var location = '&location.within=5mi&location.latitude=' + $scope.latitude + '&location.longitude='
+            + $scope.longitude + '&popular=on';
 
         //setting category 103--music
         var searchQuery = url + '?categories=103' + location +
             '&token=' + token + '&expand=venue,category';
+        $scope.currentQuery = searchQuery;
         $scope.code = null;
         $scope.response = null;
         $scope.method = 'GET';
 
-        //to fetch all the events
-        $http({method: $scope.method, url: searchQuery, cache: $templateCache}).
-        then(function(response) {
-            $scope.status = response.status;
-            $scope.data = response.data;
-            if ($scope.data != null) {
-                processData();
-                resetMarkers();
-            }
-        }, function(response) {
-            $scope.data = response.data || "Request failed";
-            $scope.status = response.status;
-        });
+        search(searchQuery,true);
     }
 
-    function processData(){
+    //method to pre-process the data retreived from the data api
+    function processData(doPages) {
         $scope.categories = [];
-        $scope.pages = [];
+
         for (var i = 0; i < $scope.data.events.length; i++) {
-            if ($scope.categories.indexOf($scope.data.events[i].category.name) == -1)
-                $scope.categories.push($scope.data.events[i].category.name);
+            if ($scope.data.events[i].category != null) {
+                if ($scope.categories.indexOf($scope.data.events[i].category.name) == -1)
+                    $scope.categories.push($scope.data.events[i].category.name);
+            }
         }
-        for (var j = 1; j <= $scope.data.pagination.page_count; j++) {
-            $scope.pages.push(j);
+        if (doPages) {
+            $scope.currentPage = 1;
+            $scope.pages = [];
+            for (var j = 1; j <= $scope.data.pagination.page_count; j++) {
+                $scope.pages.push(j);
+            }
         }
     }
-    $scope.fetchShowEvents = function(){
+
+    $scope.fetchShowEvents = function () {
         //alert("In fetchShowEvents method with parameter-->"+$scope.query);
         //var returnValue= fetch();
-        loadEvents=false;
+        loadEvents = false;
         //$scope.fetch();
-            //alert("setting url");
-            $location.url("/events/");
+        //alert("setting url");
+        $location.url("/events/");
 
     };
 
     //method to fetch the events searced for
-    $scope.fetch = function() {
-        //alert("in fetch method--->"+$scope.query);
-        var url= 'https://www.eventbriteapi.com/v3//events/search/';
+    $scope.fetch = function () {
+        var url = 'https://www.eventbriteapi.com/v3//events/search/';
         var token = 'JJJKFTCUFVWB2HPKT2DS';
         var token2 = 'QC44X66MUP27NDX7MDZL';
-        var location = '&location.within=5mi&location.latitude='+$scope.latitude+
-            '&location.longitude='+$scope.longitude+'&popular=on';
-        //*var batchurl = "https://www.eventbriteapi.com/v3/batch/" + '?token=' + token;
-        //var venueURL = '{"method":"GET", "relative_url":"venues/';
+        var location = '&location.within=5mi&location.latitude=' + $scope.latitude +
+            '&location.longitude=' + $scope.longitude + '&popular=on';
 
         var searchQuery = url + '?q=' + $scope.query + location +
             '&token=' + token + '&expand=venue,category';
-        //var venues = [];
-        //var batchRequest = '[' + venueURL;
+        $scope.currentQuery = searchQuery;
         $scope.code = null;
         $scope.response = null;
         $scope.method = 'GET';
 
-        //to fetch all the events
-        $http({method: $scope.method, url: searchQuery, cache: $templateCache}).
-        then(function(response) {
-            $scope.status = response.status;
-            $scope.data = response.data;
-            if ($scope.data != null){
-                resetMarkers();
-                processData();
-            }
-        }, function(response) {
-            $scope.data = response.data || "Request failed";
-            $scope.status = response.status;
-        });
+        search(searchQuery,true);
 
     };
 
+    //method to perform page navigation
+    $scope.fetchNextPage = function (num) {
+        if (num > 0 && num <= $scope.pages.length) {
+            $scope.currentPage = num;
+            var searchQuery = $scope.currentQuery;
+            searchQuery = searchQuery + '&page=' + num;
+            $scope.code = null;
+            $scope.response = null;
+            $scope.method = 'GET';
+
+            search(searchQuery,false);
+        }
+    };
+
+    //perform all search operations
+    //searchQuery: query to be posted
+    //page: flag to tell if pagination has to rendered
+    function search(searchQuery,page){
+        $http({method: $scope.method, url: searchQuery, cache: $templateCache}).
+        then(function (response) {
+            $scope.status = response.status;
+            $scope.data = response.data;
+            if ($scope.data != null) {
+                resetMarkers();
+                processData(page);
+            }
+        }, function (response) {
+            $scope.data = response.data || "Request failed";
+            $scope.status = response.status;
+        });
+    }
 
     //to set event markers
     function setMarkers() {
@@ -191,8 +216,8 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
                 });
 
                 /*marker.addListener('mouseover', function() {
-                    infowindow.open(map, marker);
-                });*/
+                 infowindow.open(map, marker);
+                 });*/
                 markers.push(marker);
             }
         }
@@ -200,17 +225,19 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
         setMarkerListeners();
     }
 
+    //add click listeners to all the markers
     function setMarkerListeners() {
 
-        for (var i=0; i<markers.length; i++) {
+        for (var i = 0; i < markers.length; i++) {
             var event = $scope.data.events[i];
             var marker = markers[i];
             var infowindow = new google.maps.InfoWindow();
-            bindInfoWindow(marker,infowindow,event,i);
+            bindInfoWindow(marker, infowindow, event, i);
         }
     }
 
-    function bindInfoWindow(marker, infowindow, event,num) {
+    //bind info windows to all the event markers
+    function bindInfoWindow(marker, infowindow, event, num) {
         google.maps.event.addListener(marker, 'click', (function (marker, num) {
             return function () {
 
@@ -228,12 +255,13 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
     }
 
 
+    //method to calculate and display the route
     function calculateAndDisplayRoute(destination) {
         directionsService.route({
             origin: userMarker.getPosition(),
             destination: destination,
             travelMode: google.maps.TravelMode.DRIVING
-        }, function(response, status) {
+        }, function (response, status) {
             if (status === google.maps.DirectionsStatus.OK) {
                 directionsDisplay.setDirections(response);
             } else {
@@ -243,8 +271,8 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
     }
 
     //reset any previous markers if available
-    function resetMarkers(){
-        for (var i=0; i<markers.length; i++) {
+    function resetMarkers() {
+        for (var i = 0; i < markers.length; i++) {
 
             markers[i].setMap(null);
         }
@@ -252,13 +280,15 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
         setMarkers();
     }
 
-    function hideOthers(id){
+    //method to hide all events when one is chosen
+    function hideOthers(id) {
         $('.eventRow').hide();
-        $('#'+id).show();
+        $('#' + id).show();
         $('#showAll').show();
     }
 
-    $scope.showall = function(){
+    //method to show all the events
+    $scope.showall = function () {
         map.setZoom(12);
         map.setCenter(userMarker.getPosition());
         directionsService = null;
@@ -267,11 +297,6 @@ app.controller("EventsController", function ($scope, $http, $rootScope, $locatio
         $('#showAll').hide();
     };
 
-    $scope.getNumber = function(num){
-        //alert(num);
-        if(num !=undefined)
-            return new Array(+num);
-    }
 
 });
 
