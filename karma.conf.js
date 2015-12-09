4f8d97ed4794aa96538a0eaf5dac6a42772ebb93
Abhishek Ravi Chandran <abhi2488@ccs.neu.edu>
2015-12-08 19:22:40 -0500

    changes for jenkins karma run...added chai

--- karma.conf.js
+++ karma.conf.js
@@ -1,86 +1,87 @@
 // Karma configuration
 // Generated on Sun Dec 06 2015 21:52:57 GMT-0500 (Eastern Standard Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-        'public/js/angular.js',
-      'public/js/angular-mocks.js',
-      'public/js/angular-route.js',
-      'public/js/ui-bootstrap-tpls-0.12.1.js',
-      'public/js/ng-password-strength.min.js',
-        'public/js/sociali.js',
-      'public/services/userService.js',
-      'public/services/myEventsService.js',
-      'public/services/preferenceService.js',
-        'public/js/jquery-2.1.4.min.js',
-          'public/views/**/*.js',
-          'public/test/**/*.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'public/test/js/google.js',
+            'public/js/angular.js',
+            'public/js/angular-mocks.js',
+            'public/js/angular-route.js',
+            'public/js/angular-animate.min.js',
+            'public/js/ui-bootstrap-tpls-0.12.1.js',
+            'public/js/ng-password-strength.min.js',
+            'public/js/sociali.js',
+            'public/services/userService.js',
+            'public/services/myEventsService.js',
+            'public/services/preferenceService.js',
+            'public/js/jquery-2.1.4.min.js',
+            'public/views/**/*.js',
+            'public/test/**/*.js'
+
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    //reporters: ['progress'],
-    reporters : ['dots', 'junit'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        //reporters: ['progress'],
+        reporters: ['dots', 'junit'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
-    // Concurrency level
-    // how many browser should be started simultanous
-    concurrency: Infinity,
 
-    junitReporter : {
-    outputFile: 'test-results.xml'
-    }
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
 
-  })
+        // Concurrency level
+        // how many browser should be started simultanous
+        concurrency: Infinity,
+
+        junitReporter: {
+            outputFile: 'test-results.xml'
+        }
+
+    })
 };
